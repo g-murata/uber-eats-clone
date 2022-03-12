3489f282d61b7d08a98163e7da2e1fe8ff993eba
ta-mu-g-m <jojo1234dorararara@gmail.com>
2022-03-12 17:02:12 +0900

    フード一覧のAPIを叩く

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,14 +1,24 @@
-import React, { Fragment } from 'react';
+// --- 次にuseEffectを追加 ---
+import React, { Fragment, useEffect } from 'react';
+
+// --- ここから追加 ---
+// apis
+import { fetchFoods } from '../apis/foods';
+// --- ここまで追加 ---
 
 export const Foods = ({
   match
 }) => {
+  useEffect(() => {
+    fetchFoods(match.params.restaurantsId)
+      .then((data) =>
+        console.log(data)
+      )
+  }, [])
+
   return (
     <Fragment>
       フード一覧
-      <p>
-        restaurantsIdは {match.params.restaurantsId} です
-      </p>
     </Fragment>
   )
 }
