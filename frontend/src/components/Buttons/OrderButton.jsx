6a008b543a9587919a6a4aa3c081cf3ddbd70881
ta-mu-g-m <jojo1234dorararara@gmail.com>
2022-03-20 10:27:40 +0900

    MOD_OrderButtonにCountDownButtonを定義しとる

--- frontend/src/components/Buttons/OrderButton.jsx
+++ frontend/src/components/Buttons/OrderButton.jsx
@@ -1,15 +1,20 @@
 // --- ここから追加 ---
-import React from 'react';
+import styled from 'styled-components';
 
-// style
-import { RoundButton } from '../shared_style';
+// components
+import { BaseButton } from '../shared_style';
 
-export const CountDownButton = ({
-  onClick,
-  isDisabled,
-}) => (
-  <RoundButton onClick={onClick} disabled={isDisabled}>
-    ー
-  </RoundButton>
-)
+// constants
+import { FONT_SIZE } from '../../style_constants';
+
+// 関数コンポーネントではなく、styled-componentsで作った関数
+// propsや関数が不要な場合にはこれだけでもOK
+export const OrderButton = styled(BaseButton)`
+  width: 390px;
+  background-color: black;
+  color: white;
+  border-style: none;
+  padding: 8px 16px;
+  font-size: ${FONT_SIZE.BODY1};
+`;
 // --- ここまで追加 ---
